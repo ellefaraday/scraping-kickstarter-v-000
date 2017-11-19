e0e1614eb5a62a50cc6e44d31e9b65424d7e8375
Elle Faraday <faraday.elle@gmail.com>
2017-11-19 01:01:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,11 +9,13 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] =
-      {:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta li a").text,
-      :percent_funded => project.css("ul.project-stats li.first strong").text.gsub("%", "").to_i}
+      :percent_funded => project.css("ul.project-stats li.first strong").text.gsub("%", "").to_i
+    }
   end
 
   #projects = kickstarter.css("li.project.grid_4")
